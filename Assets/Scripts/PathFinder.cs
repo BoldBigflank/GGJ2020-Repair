24c115ebaa05683a28b94db980d7a9ba2eea3a18
BradStar879 <bradstar879@yahoo.com>
2020-02-01 16:37:13 -0600

    Simplified spline code

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -4,34 +4,14 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
-    [SerializeField] GameObject path;
     [SerializeField] float speed = 1f;
-    [SerializeField] int interpolationsBetweenPoints = 10; //Make this even or it might break
-    List<Transform> nodes;
     int nextPointNum;
     Vector3 nextPoint;
-    [SerializeField] LineRenderer lineRenderer;
-    private Vector3[] pathPositions;
+    Vector3[] pathPositions;
 
     void Start()
     {
-        nodes = new List<Transform>();
-        bool skipFirst = false; //First child transform will be path transfrom itself
-        foreach (Transform node in path.GetComponentsInChildren<Transform>())
-        {
-            if (!skipFirst)  //This would be cleaner if it was an enum but oh well
-            {
-                skipFirst = true;
-            }
-            else
-            {
-                nodes.Add(node);
-            }
-        }
-        int totalInterpolations = nodes.Count * (interpolationsBetweenPoints / 2) - (interpolationsBetweenPoints / 2 - 1);
-        lineRenderer.positionCount = totalInterpolations;
-        pathPositions = new Vector3[totalInterpolations];
-        DrawCurve();
+        pathPositions = GameObject.FindGameObjectWithTag("Assembly Path").GetComponent<PathCreator>().GetPathPositions();
         nextPointNum = 0;
         nextPoint = pathPositions[0];
     }
@@ -44,7 +24,6 @@ public class PathFinder : MonoBehaviour
 
     private void FixedUpdate()
     {
-        DrawCurve();
         if (Vector2.Distance(transform.position, nextPoint) < .05f)
         {
             nextPointNum++;
@@ -58,30 +37,4 @@ public class PathFinder : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, nextPoint, step);
     }
 
-    private void DrawCurve()
-    {
-        int nodeCount = 0;
-        for (int i = 0; i < pathPositions.Length - 1; i++)
-        {
-            if (i % interpolationsBetweenPoints == 0 && i != 0)
-            {
-                nodeCount += 2;
-            }
-            float t = (i % interpolationsBetweenPoints) / (float) interpolationsBetweenPoints;
-            pathPositions[i] = CalculateBezierPoint(t, nodes[nodeCount].position, nodes[nodeCount + 1].position, nodes[nodeCount + 2].position);
-        }
-        pathPositions[pathPositions.Length - 1] = nodes[nodes.Count - 1].position;
-        lineRenderer.SetPositions(pathPositions);
-    }
-
-    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
-    {
-        float u = 1 - t;
-        float tt = t * t;
-        float uu = u * u;
-        Vector3 p = uu * p0;
-        p += 2 * u * t * p1;
-        p += tt * p2;
-        return p;
-    }
 }
