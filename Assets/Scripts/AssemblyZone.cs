34e42f217f2717c7be5dfaa9c82ac1adec293fb7
TortoisEatToes <saralynnasmussen@gmail.com>
2020-02-01 12:40:29 -0600

    First Mechanic Prototype Done
    Two players can now run around in a scene, pick up, and rop off objects.

---
+++ Assets/Scripts/AssemblyZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssemblyZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DropOffInteractable(Collider2D col)
+    {
+        Debug.Log("A player gave the assembly zone an interactable");
+    }
+}
---
+++ Assets/Scripts/InputComponentController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComponentController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player;
+    [SerializeField]
+    private string inputXAxis = "Controller1X";
+    [SerializeField]
+    private string inputYAxis = "Controller1Y";
+    [SerializeField]
+    private string inputInteractButton = "Controller1Interact";
+
+    private bool isIteractButtonDown = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        player.Move(Input.GetAxis(inputXAxis), Input.GetAxis(inputYAxis));
+        DetectInteractButtonState();
+        //Debug.Log(Input.GetAxis(inputInteractButton));
+        //Debug.Log(Input.GetKeyDown("joystick 1 button 1"));
+    }
+
+    private void DetectInteractButtonState()
+    {
+        if(isIteractButtonDown)
+        {
+            if(Input.GetAxis(inputInteractButton) == 0.0f) //Button was released
+            {
+                isIteractButtonDown = false;
+                //Debug.Log(Input.GetAxis(inputInteractButton));
+            }
+        }
+        else
+        {
+            if(Input.GetAxis(inputInteractButton) > 0.0f) //Button was pressed
+            {
+                player.Interact();
+                //Debug.Log(Input.GetAxis(inputInteractButton));
+                isIteractButtonDown = true;
+            }   
+        }
+    }
+}
