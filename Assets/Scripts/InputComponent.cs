f4bca62815dcf71dbf1e6621446da258b5906aff
TortoisEatToes <saralynnasmussen@gmail.com>
2020-02-01 10:41:54 -0600

    Added scripts and prefabs folder and settings

---
+++ Assets/Scripts/InputComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+    public PlayerController player;
+    public string playerName = "1";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        player.Move(Input.GetAxisRaw("JoyStick" + playerName + "X"), Input.GetAxisRaw("JoyStick" + playerName + "Y"));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float x, float y)
+    {
+        transform.Translate(x * moveSpeed, 0.0f, 0.0f);
+        transform.Translate(0.0f, y * moveSpeed, 0.0f);
+
+        Debug.Log("X Value: " + x);
+        //Debug.Log("Y Value: " + y);
+    }
+
+    public void Interact()
+    {
+
+    }
+}
