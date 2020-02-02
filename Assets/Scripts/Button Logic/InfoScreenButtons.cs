74f12dad7d1fa93dede39f6c6aaee0269f917689
TortoisEatToes <saralynnasmussen@gmail.com>
2020-02-02 01:26:30 -0600

    Added an info screen to the main menu
    Plus more work towards getting objects to be destroyed by the holes

---
+++ Assets/Scripts/Button Logic/InfoScreenButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoScreenButtons : MonoBehaviour
+{
+    public GameObject mainScreen;
+    public GameObject infoScreen;
+
+    public void DisplayeMainScreen()
+    {
+        mainScreen.SetActive(true);
+        infoScreen.SetActive(false);
+    }
+
+    public void DisplayeInfoScreen()
+    {
+        mainScreen.SetActive(false);
+        infoScreen.SetActive(true);
+    }
+}
