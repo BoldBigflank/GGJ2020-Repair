f06dab6f2d249f687d063f8e71470a555d0994a9
BradStar879 <bradstar879@yahoo.com>
2020-02-02 07:48:08 -0600

    Added 5th bonus and properly get bonusCount

--- Assets/Scripts/ScoreCard.cs
+++ Assets/Scripts/ScoreCard.cs
@@ -21,6 +21,7 @@ public class ScoreCard
         int penaltyMatches = 0;
         int stolenParts = 0;
         int totalParts = 0;
+        int[] animalCount = new int[30];    //Will grow if more than 30 animals are added
         foreach (BodyPart bodyPart in bodyParts)
         {
             if (bodyPart.animalType == targetAnimal)
@@ -36,28 +37,42 @@ public class ScoreCard
                 stolenParts++;
             }
             totalParts++;
+            animalCount[(int)bodyPart.animalType]++;
         }
         if (targetMatches > 0)
         {
             Bonus targetBonus = new Bonus(Bonus.BonusType.target, targetMatches);
             bonuses.AddLast(targetBonus);
+            bonusSize++;
         }
         if (penaltyMatches > 0)
         {
             Bonus penaltyBonus = new Bonus(Bonus.BonusType.penalty, penaltyMatches);
             bonuses.AddLast(penaltyBonus);
+            bonusSize++;
         }
         if (stolenParts > 0)
         {
             Bonus stolenBonus = new Bonus(Bonus.BonusType.stolenParts, stolenParts);
             bonuses.AddLast(stolenBonus);
+            bonusSize++;
         }
         if (totalParts < 5)
         {
             Bonus missingPenalty = new Bonus(Bonus.BonusType.missingParts, 5 - totalParts);
             bonuses.AddLast(missingPenalty);
+            bonusSize++;
         }
-        bonusSize = bonuses.Count;
+        for (int i = 0; i < 30; i++)
+        {
+            if (animalCount[i] >= 3)
+            {
+                Bonus matchingBonus = new Bonus(Bonus.BonusType.matchingParts, animalCount[i]);
+                bonuses.AddLast(matchingBonus);
+                bonusSize++;
+                break;
+            }
+        }
         totalScore = 0;
         foreach (Bonus bonus in bonuses)
         {
