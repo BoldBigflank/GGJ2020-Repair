9a81d489aeb9be628c0c12093ae75068043ddf8d
TortoisEatToes <saralynnasmussen@gmail.com>
2020-02-01 23:38:56 -0600

    Creating the game state manager
    This should help carry data between scenes

---
+++ Assets/Scripts/AssemblyZonesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssemblyZonesController : MonoBehaviour
+{
+    public AssemblyZone zone1;
+    public AssemblyZone zone2;
+    public AssemblyZone zone3;
+    public AssemblyZone zone4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStateManager.InitializeP1(zone1);
+        GameStateManager.InitializeP2(zone2);
+        GameStateManager.InitializeP3(zone3);
+        GameStateManager.InitializeP4(zone4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
