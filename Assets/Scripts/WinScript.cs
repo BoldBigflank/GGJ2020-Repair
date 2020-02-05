b7f407312b99eab1e9391ad033e91e85e6f83eae
bradstar879 <bradstar879@yahoo.com>
2020-02-04 22:58:50 -0600

    Added missing assets

---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] winSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int winningPlayer = 0;
+        int winningScore = -50;
+        for (int i = 0; i < 4; i++)
+        {
+            if (i < GameStateManager.GetNumberOfPlayers() && GameStateManager.GetPlayerScore(i) > winningScore)
+            {
+                winningScore = GameStateManager.GetPlayerScore(i);
+                winningPlayer = i;
+            }
+            winSprites[i].GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
