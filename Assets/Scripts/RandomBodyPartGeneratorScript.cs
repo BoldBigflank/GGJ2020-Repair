bfee11fc5ad21d157266612df7c214e6584916f3
bradstar879 <bradstar879@yahoo.com>
2020-02-09 00:19:11 -0600

    Changed body part generator and organized main game

--- Assets/Scripts/RandomBodyPartGeneratorScript.cs
+++ Assets/Scripts/RandomBodyPartGeneratorScript.cs
@@ -4,58 +4,14 @@ using UnityEngine;
 
 public class RandomBodyPartGeneratorScript : MonoBehaviour
 {
-    public GameObject bodyPartHeadCat;
-    public GameObject bodyPartHeadChameleon;
-    public GameObject bodyPartHeadCow;
+    [SerializeField] GameObject[] bodyParts;
+    [SerializeField] GameObject[] heads;
 
-    public GameObject bodyPartHeadDog;
-
-    public GameObject bodyPartHeadFrog;
-    public GameObject bodyPartHeadGirraffe;
-    public GameObject bodyPartHeadHamster;
-    public GameObject bodyPartHeadKoala;
-
-    public GameObject bodyPartHeadOctopus;
-    public GameObject bodyPartHeadToucan;
-    
-    public GameObject bodyPartFeetCatFront;
-    public GameObject bodyPartFeetCatBack;
-
-    public GameObject bodyPartFeetChameleon;
-
-    public GameObject bodyPartFeetCowBack;
-    public GameObject bodyPartFeetCowFront;
-
-    public GameObject bodyPartFeetFrogBack;
-    public GameObject bodyPartFeetFrogFront;
-
-    public GameObject bodyPartFeetGiraffeBack;
-    public GameObject bodyPartFeetGiraffeFront;
-
-    public GameObject bodyPartFeetKoalaBack;
-    public GameObject bodyPartFeetKoalaFront;
-
-    public GameObject bodyPartFeetLemurFront;
-
-    public GameObject bodyPartFeetLemurBack;
-
-    public GameObject bodyPartFeetOctopusBack;
-    public GameObject bodyPartFeetOctopusFront;
-
-    public GameObject bodyPartFeetToucanBack;
-    public GameObject bodyPartFeetToucanFront;
-
-    public GameObject bodyPartFeetToucanWing;
-
-
-        public Transform lowerConveyorSpawn;
-        public Transform upperConveyorSpawn;
-        public float timeBetweenSpawn = 2.0f;
-        private float elapsedTime = 0.0f;
-        private int randomStartingPoint;
-
-
-
+    public Transform lowerConveyorSpawn;
+    public Transform upperConveyorSpawn;
+    public float timeBetweenSpawn = 2.0f;
+    private float elapsedTime = 0.0f;
+    private int randomStartingPoint;
 
     void Start()
     {
@@ -81,149 +37,22 @@ public class RandomBodyPartGeneratorScript : MonoBehaviour
         }
     }
 
-    public GameObject CreateRandomBodyPart(Vector3 startingPosition, bool spawnUpper)
+    public void CreateRandomBodyPart(Vector3 startingPosition, bool spawnUpper)
     {
-        int randomValue;
-        if (Random.Range(0, 100) < 21)
+        GameObject bodyPartToSpawn;
+        if (Random.Range(0, 10) < 3)    // 30% chance of a head spawning
         {
-            randomValue = Random.Range(0, 10);
+            bodyPartToSpawn = Instantiate(heads[Random.Range(0, heads.Length)], startingPosition, Quaternion.identity);
         }
         else
         {
-            randomValue = Random.Range(10, 29);
-        }
-        GameObject temp;
-
-        switch(randomValue)
-        {
-                case 0:
-                temp =  Instantiate(bodyPartHeadCat, startingPosition, Quaternion.identity);
-                break;
-                
-                case 1:
-                temp =  Instantiate(bodyPartHeadChameleon, startingPosition, Quaternion.identity);
-                break;
-
-                case 2:
-                temp =  Instantiate(bodyPartHeadCow, startingPosition, Quaternion.identity);
-                break;
-
-                case 3:
-                temp =  Instantiate(bodyPartHeadDog, startingPosition, Quaternion.identity);
-                break;
-
-                case 4:
-                temp =  Instantiate(bodyPartHeadFrog, startingPosition, Quaternion.identity);
-                break;
-
-                case 5:
-                temp =  Instantiate(bodyPartHeadGirraffe, startingPosition, Quaternion.identity);
-                break;
-
-                case 6:
-                temp =  Instantiate(bodyPartHeadHamster, startingPosition, Quaternion.identity);
-                break;
-
-                case 7:
-                temp =  Instantiate(bodyPartHeadKoala, startingPosition, Quaternion.identity);
-                break;
-
-                case 8:
-                temp =  Instantiate(bodyPartHeadToucan, startingPosition, Quaternion.identity);
-                break;
-
-                case 9:
-                temp =  Instantiate(bodyPartFeetCatFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 10:
-                temp =  Instantiate(bodyPartHeadOctopus, startingPosition, Quaternion.identity);
-                break;
-
-                case 11:
-                temp =  Instantiate(bodyPartFeetCatBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 12:
-                temp =  Instantiate(bodyPartFeetChameleon, startingPosition, Quaternion.identity);
-                break;
-
-                case 13:
-                temp =  Instantiate(bodyPartFeetCowBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 14:
-                temp =  Instantiate(bodyPartFeetCowFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 15:
-                temp =  Instantiate(bodyPartFeetFrogBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 16:
-                temp =  Instantiate(bodyPartFeetFrogFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 17:
-                temp =  Instantiate(bodyPartFeetLemurFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 18:
-                temp =  Instantiate(bodyPartFeetLemurBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 19:
-                temp =  Instantiate(bodyPartFeetOctopusBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 20:
-                temp =  Instantiate(bodyPartFeetOctopusFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 21:
-                temp =  Instantiate(bodyPartFeetToucanBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 22:
-                temp =  Instantiate(bodyPartFeetToucanBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 23:
-                temp =  Instantiate(bodyPartFeetOctopusBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 24:
-                temp =  Instantiate(bodyPartFeetGiraffeBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 25:
-                temp =  Instantiate(bodyPartFeetGiraffeFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 26:
-                temp =  Instantiate(bodyPartFeetKoalaFront, startingPosition, Quaternion.identity);
-                break;
-
-                case 27:
-                temp =  Instantiate(bodyPartFeetKoalaBack, startingPosition, Quaternion.identity);
-                break;
-
-                case 28:
-                temp =  Instantiate(bodyPartFeetToucanWing, startingPosition, Quaternion.identity);
-                break;
-
-                default:
-                temp =  Instantiate(bodyPartFeetToucanFront, startingPosition, Quaternion.identity);
-                break;
-        }
-
-        if(spawnUpper)
-        {
-                temp.GetComponent<SpriteRenderer>().sortingOrder = 7;
-                temp.GetComponent<PathFinder>().SetNextPoint(35);
+            bodyPartToSpawn = Instantiate(bodyParts[Random.Range(0, bodyParts.Length)], startingPosition, Quaternion.identity);
         }
         
-
-        return temp;
+        if(spawnUpper)
+        {
+            bodyPartToSpawn.GetComponent<SpriteRenderer>().sortingOrder = 7;
+            bodyPartToSpawn.GetComponent<PathFinder>().SetNextPoint(35);
+        }
     }
 }
