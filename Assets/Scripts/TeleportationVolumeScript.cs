4b5ab2d825a26fce1aa29ef7d22a9170b76a8306
bpatterson <bradley.patterson@topbloc.com>
2020-02-10 01:01:04 -0600

    Added any missing files

---
+++ Assets/Scripts/TeleportationVolumeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationVolumeScript : MonoBehaviour
+{
+    private Transform teleportationPoint;
+    private Vector2 teleporationVector;
+    [SerializeField] bool sidePortal;   //True if left/right, false if up/down
+
+    void Start()
+    {
+        teleportationPoint = transform.GetChild(0);
+        teleporationVector = new Vector2(teleportationPoint.position.x, teleportationPoint.position.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<PlayerController>().Teleport(teleporationVector, sidePortal);
+        }
+    }
+
+}
