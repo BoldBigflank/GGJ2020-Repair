2a5e9f59bd78106a0e139d25129a171df0e53d2c
bpatterson <bradley.patterson@topbloc.com>
2020-02-22 14:04:30 -0600

    Fixed input issues

---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+
+    private ControllerState[] controllerStates;
+    [SerializeField] int maxContollers = 4;
+    private int connectedControllers = 0;
+    // Start is called before the first frame update
+    public void Initialize()
+    {
+        controllerStates = new ControllerState[maxContollers];
+        for (int i = 0; i < maxContollers; i++)
+        {
+            controllerStates[i] = new ControllerState();
+            controllerStates[i].SetInputSpot(i + 1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        int inputSpot = 1;
+        int controllerNumber = 0;
+        foreach (string controllerName in Input.GetJoystickNames())
+        {
+            if (controllerName.Length > 0)
+            {
+                controllerStates[controllerNumber].SetInputSpot(inputSpot);
+                controllerNumber++;
+            }
+            inputSpot++;
+            if (controllerNumber >= maxContollers)
+            {
+                break;
+            }
+        }
+        connectedControllers = controllerNumber;
+    }
+
+    public ControllerState GetControllerState(int controllerNumber)
+    {
+        return controllerStates[controllerNumber];
+    }
+
+    public int GetConnectedControllers()
+    {
+        return connectedControllers;
+    }
+
+}
---
+++ Assets/Scripts/ControllerState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerState : MonoBehaviour
+{
+    private int inputSpot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputSpot = 1;
+    }
+
+    public void SetInputSpot(int newControllerNumber)
+    {
+        inputSpot = newControllerNumber;
+    }
+
+    public float GetXAxis()
+    {
+        return Input.GetAxis("Controller" + inputSpot + "_X");
+    }
+
+    public float GetYAxis()
+    {
+        return Input.GetAxis("Controller" + inputSpot + "_Y");
+    }
+
+    public bool GetAButtonDown()
+    {
+        return Input.GetButtonDown("Controller" + inputSpot + "_A");
+    }
+}
